2d1e90e642445abe5d19284ac503004fb85ef98e
Marek Rychlik <rychlik@arizona.edu>
2026-03-12 18:54:15 -0700

    *** empty log message ***

---
+++ src/python/student.py
@@ -0,0 +1,13 @@
+# 
+# In this module implement these two functions:
+# 1. solve_continuous_are
+# 2. solve_ivp
+#
+# Make sure that they are compatible with their usage
+# in modal_lqr.py.
+#
+# The Gradescope Autograder will call your implementation
+# through functions:
+#
+# 1. simulate_closed_loop
+# 2. simulate_open_loop
